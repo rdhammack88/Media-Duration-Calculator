227bae625414701f2ffb135fbd4de7ad6fd011b8
Dustin Hammack <rdhammack88@hotmail.com>
2018-04-10 03:20:11 -0500

    done feature added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $('document').ready(function() {
 
     var infoBody = $('#file-info');
+    var fileNamesList = [], fileList = [];
     var fileInfo = {
         index: 0,
 
@@ -10,7 +11,31 @@ $('document').ready(function() {
 
 
         display: function(index, name, type, len=0) {
-            return `<tr><th scope="row">${index}</th><td class="fileName">${name}</td><td>${type}</td><td>${len}</td><td class="icons"><button type="button"><i class="fas fa-trash"></i></button><button type="button"><i class="fas fa-check"></i></button></td></tr>` //&nbsp;&nbsp;&nbsp;&nbsp;
+            return `<tr><th scope="row">${index}</th><td class="fileName">${name}</td><td>${type}</td><td>${len}</td><td class="icons"><button type="button" class="delete"><i class="fas fa-trash"></i></button><button type="button" class="done"><i class="fas fa-check"></i></button></td></tr>`;
+        },
+
+        getFileDuration: function() {
+            var files = this.files;
+            // for(var i = 0; i < this.length; i++) {
+                console.log(this.files);
+                // fileList.push(files[i]);
+                var video = document.createElement('video');
+                video.preload = 'metadata';
+
+                video.onloadedmetadata = function() {
+                    window.URL.revokeObjectURL(video.src);
+                    var duration = video.duration;
+                    fileList[fileList.length - 1].duration = duration
+                }
+                video.src = URL.createObjectURL(files[0]);
+            // }
+            return fileList;
+        },
+
+        deleteFileInfo: function() {
+            return console.log('clicked');
+            var that = this;
+            var indx = $(this).parent('tr').eq();
         }
 
 
@@ -29,19 +54,22 @@ $('document').ready(function() {
         console.log(this.files[0].name);
         console.log(this.files);
 
-        var fileNames = document.querySelectorAll('.fileName');
-        fileNames = fileNames.length ? Array.from(fileNames) : [];
-        console.log(fileNames);
+        // var fileNames = document.querySelectorAll('.fileName');
+        // fileNamesList = fileNames.length ? Array.from(fileNames) : [];
+
+        console.log(fileNamesList);
         var totalFileCount = $('table > tbody > tr:last-of-type > th').html();
         if(!totalFileCount) {
             totalFileCount = 0;
         }
 
         for(var i = 0; i < this.files.length; i++) {
-            if(fileNames.includes(this.files[i].name)) {
+            if(fileNamesList.includes(this.files[i].name)) {
                 continue;
             } else {
-                fileNames.push(this.files[i].name);
+                // var fileList = fileInfo.getFileDuration();
+                // console.log(fileList);
+                fileNamesList.push(this.files[i].name);
                 infoBody.append(fileInfo.display(++totalFileCount, this.files[i].name, this.files[i].type));
             }
         }
@@ -49,4 +77,25 @@ $('document').ready(function() {
         $('#totalFileCount').text(`${totalFileCount} `);
 
     });
+
+    $('body').on('click', '.delete', function(e) {
+        e.preventDefault();
+        console.log($(this).eq());
+        console.log($(this).parents('tr'));
+        $(this).parents('tr').addClass('remove');
+        // $('.remove').parents('tbody').remove('.remove');
+        $(this).parents('tbody').remove('.remove');
+        // var that = this;
+        // var indx = $(this).parent('tr').eq();
+    });
+
+    $('body').on('click', '.done', function(e) {
+        e.preventDefault();
+        $(this).css({'color':'green'});
+        $(this).parents('tr').children('td').css({
+            'text-decoration': 'line-through',
+            'color': 'green'
+        });
+    });
+
 });
